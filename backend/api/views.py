9eaf469268486bb89e8d210580d6de595f8e08c0
André Marques <2231110@my.ipleiria.pt>
2026-05-18 12:51:48 +0100

    Crud of classes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,13 @@
 from django.http import JsonResponse
+from rest_framework import viewsets
+from .models import Class
+from .serializers import ClassSerializer
 
 def hello(request):
     return JsonResponse({
         'message': 'Backend Django a funcionar!'
     })
+
+class ClassViewSet(viewsets.ModelViewSet):
+    queryset = Class.objects.all()
+    serializer_class = ClassSerializer
